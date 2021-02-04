8bbca50ad0e2124450a363007a68b36598543d6f
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-02-04 11:36:22 -0300

    Criando repository e service para Categoria

--- src/main/java/com/dhyegopedroso/cursosb/resources/CategoriasResource.java
+++ src/main/java/com/dhyegopedroso/cursosb/resources/CategoriasResource.java
@@ -1,29 +1,27 @@
 package com.dhyegopedroso.cursosb.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dhyegopedroso.cursosb.domain.Categoria;
+import com.dhyegopedroso.cursosb.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriasResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1,"Informatica");
-		Categoria cat2 = new Categoria(2,"Escritorio");
-		
-		List<Categoria> list = new ArrayList<>();
-		list.addAll(Arrays.asList(cat1, cat2));
-		
-		return list;
+	@Autowired
+	private CategoriaService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
