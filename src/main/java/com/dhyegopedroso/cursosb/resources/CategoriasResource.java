3a3a8a5feb271a03cb387977ed3cf8d3c935b741
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-02-04 10:59:06 -0300

    REST testado

---
+++ src/main/java/com/dhyegopedroso/cursosb/resources/CategoriasResource.java
@@ -0,0 +1,16 @@
+package com.dhyegopedroso.cursosb.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriasResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "Resta esta funfando";
+	}
+
+}
