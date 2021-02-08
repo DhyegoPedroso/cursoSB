48934c0770d37fa92be1dd9042fb3d0c05c79466
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-02-08 10:42:17 -0300

    Criando operacao de instanciacao

--- src/main/java/com/dhyegopedroso/cursosb/CursosbApplication.java
+++ src/main/java/com/dhyegopedroso/cursosb/CursosbApplication.java
@@ -1,13 +1,32 @@
 package com.dhyegopedroso.cursosb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dhyegopedroso.cursosb.domain.Categoria;
+import com.dhyegopedroso.cursosb.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursosbApplication {
+public class CursosbApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursosbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
---
+++ src/main/java/com/dhyegopedroso/cursosb/domain/Pedido.java
@@ -0,0 +1,72 @@
+package com.dhyegopedroso.cursosb.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pedido implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+	private Date instante;
+
+	public Pedido(Integer id, Date instante) {
+		super();
+		Id = id;
+		this.instante = instante;
+	}
+
+	public Pedido() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public Date getInstante() {
+		return instante;
+	}
+
+	public void setInstante(Date instante) {
+		this.instante = instante;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+
+}
