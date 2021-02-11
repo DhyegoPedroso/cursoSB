7c7f6bae87b65510bc6ab59f97b587182ea7348d
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-02-11 12:26:23 -0300

    Validação sintática com Bean Validation:

--- src/main/java/com/dhyegopedroso/cursosb/services/CategoriaService.java
+++ src/main/java/com/dhyegopedroso/cursosb/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.dhyegopedroso.cursosb.domain.Categoria;
+import com.dhyegopedroso.cursosb.dto.CategoriaDTO;
 import com.dhyegopedroso.cursosb.repositories.CategoriaRepository;
 import com.dhyegopedroso.cursosb.services.exceptions.DataIntegrityException;
 import com.dhyegopedroso.cursosb.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 
 		return repository.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
