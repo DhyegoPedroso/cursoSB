471b2a71ad34fcfcbb250e120708f8630bd1d04b
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-02-11 11:01:24 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/dhyegopedroso/cursosb/resources/CategoriasResource.java
+++ src/main/java/com/dhyegopedroso/cursosb/resources/CategoriasResource.java
@@ -1,6 +1,8 @@
 package com.dhyegopedroso.cursosb.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dhyegopedroso.cursosb.domain.Categoria;
+import com.dhyegopedroso.cursosb.dto.CategoriaDTO;
 import com.dhyegopedroso.cursosb.services.CategoriaService;
 
 @RestController
@@ -49,6 +52,13 @@ public class CategoriasResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
