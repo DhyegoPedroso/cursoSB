2212751e15b94fdff81b7f540ed96eddcae2d8f3
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-02-27 11:02:00 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/dhyegopedroso/cursosb/services/PedidoService.java
+++ src/main/java/com/dhyegopedroso/cursosb/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dhyegopedroso.cursosb.domain.Cliente;
 import com.dhyegopedroso.cursosb.domain.ItemPedido;
 import com.dhyegopedroso.cursosb.domain.PagamentoComBoleto;
 import com.dhyegopedroso.cursosb.domain.Pedido;
@@ -14,6 +18,8 @@ import com.dhyegopedroso.cursosb.domain.enums.EstadoPagamento;
 import com.dhyegopedroso.cursosb.repositories.ItemPedidoRepository;
 import com.dhyegopedroso.cursosb.repositories.PagamentoRepository;
 import com.dhyegopedroso.cursosb.repositories.PedidoRepository;
+import com.dhyegopedroso.cursosb.security.UserSS;
+import com.dhyegopedroso.cursosb.services.exceptions.AuthorizationException;
 import com.dhyegopedroso.cursosb.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -75,6 +81,18 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repository.findByCliente(cliente, pageRequest);
 	}
 
 }
--- src/main/java/com/dhyegopedroso/cursosb/services/ProdutoService.java
+++ src/main/java/com/dhyegopedroso/cursosb/services/ProdutoService.java
@@ -13,26 +13,36 @@ import com.dhyegopedroso.cursosb.domain.Categoria;
 import com.dhyegopedroso.cursosb.domain.Produto;
 import com.dhyegopedroso.cursosb.repositories.CategoriaRepository;
 import com.dhyegopedroso.cursosb.repositories.ProdutoRepository;
+import com.dhyegopedroso.cursosb.services.exceptions.NumberFormatException;
 import com.dhyegopedroso.cursosb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ProdutoService {
-	
+
 	@Autowired
 	private ProdutoRepository repo;
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Produto find(Integer id) {
 		Optional<Produto> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
 	}
 
-	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		List<Categoria> categorias = categoriaRepository.findAllById(ids);
-		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,
+			String direction) {
+
+		try {
+
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+			List<Categoria> categorias = categoriaRepository.findAllById(ids);
+			return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+
+		} catch (Exception e) {
+			throw new NumberFormatException("Produto não encotrado");
+		}
+
 	}
 }
